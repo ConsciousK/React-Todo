8230b4618da1978d32d25f10e23cd434f51a92ec
Kevin Grant <ConsciousK@yahoo.com>
2019-06-06 21:12:13 -0400

    Removed default todo tasks for a clean form.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-      ],
+      todos: [],
       todo: ''
     };
   }
@@ -50,6 +39,7 @@ class App extends React.Component {
         return todo;
       }
     });
+    
     this.setState({ todos });
   };
 
